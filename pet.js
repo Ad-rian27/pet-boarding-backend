1e1906c5e963ae3c7f0253a9d98cd87d0d81b4e0
Ad-rian27 <rian270627@gmail.com>
2026-07-14 23:09:22 +0530

    Made connection

---
+++ pet.js
@@ -0,0 +1,42 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://adrian:adrian123@cluster0.veegpvo.mongodb.net/petdb").then(
+    () => {
+        console.log("MongoDB connected")
+    }
+).catch(
+    (err) => (
+        console.log(err)
+))
+
+const Pet=mongoose.model("Pets", new mongoose.Schema(
+    {
+        bookingID: String,
+        petName: String,
+        petType: String,
+        breed: String,
+        age: String,
+        weight: String,
+        vaccStatus: String,
+        ownerName: String,
+        phone: String,
+        email: String,
+        checkIn: String,
+        checkOut: String,
+        kenNo: String
+    }
+))
+
+app.get("/test", (req, res) => {
+    res.send("hello")
+})
+
+app.listen(3000,() => {
+    console.log("Server started")
+})
